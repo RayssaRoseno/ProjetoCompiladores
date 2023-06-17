824a1cd5ebf866bb65fc04fc78ee558b6c1c63b7
Rayssa M. Roseno <rr@ic.ufal.br>
2023-06-17 18:51:35 -0300

    Update Analisador_sintatico.py

--- analisadorSintatico/Analisador_sintatico.py
+++ analisadorSintatico/Analisador_sintatico.py
@@ -1,61 +1,58 @@
 # Tabela shift-reduce simplificada
 tabela = {
-    (0, 'programa'): 's1',
-    (1, '<id>'): 's2',
-    (2, ';'): 's3',
-    (3, 'begin'): 's4',
-    (4, 'end'): 'r5',
-    (4, '$'): 'r5',
-    (5, 'end'): 'r3',
-    (5, '$'): 'r3',
-    (6, '$'): 'accept'
+    (0, 'programa'): 's1',  # Se o estado atual é 0 e o símbolo de entrada é 'programa', realiza um "shift" para o estado 1
+    (1, '<id>'): 's2',  # Se o estado atual é 1 e o símbolo de entrada é '<id>', realiza um "shift" para o estado 2
+    (2, ';'): 's3',  # Se o estado atual é 2 e o símbolo de entrada é ';', realiza um "shift" para o estado 3
+    (3, 'begin'): 's4',  # Se o estado atual é 3 e o símbolo de entrada é 'begin', realiza um "shift" para o estado 4
+    (4, 'end'): 'r5',  # Se o estado atual é 4 e o símbolo de entrada é 'end', realiza uma "reduce" utilizando a regra 5
+    (4, '$'): 'r5',  # Se o estado atual é 4 e o símbolo de entrada é '$', realiza uma "reduce" utilizando a regra 5
+    (5, 'end'): 'r3',  # Se o estado atual é 5 e o símbolo de entrada é 'end', realiza uma "reduce" utilizando a regra 3
+    (5, '$'): 'r3',  # Se o estado atual é 5 e o símbolo de entrada é '$', realiza uma "reduce" utilizando a regra 3
+    (6, '$'): 'accept'  # Se o estado atual é 6 e o símbolo de entrada é '$', aceita a análise sintática
 }
 
-# Gramática corrigida
 regras = {
-    1: ('programa', ['<id>', ';', '<corpo>']),
-    2: ('<corpo>', ['begin', '<sentencas>', 'end']),
-    3: ('<sentencas>', ['<sentencas>', '<sentenca>']),
-    4: ('<sentencas>', []),
-    5: ('<sentenca>', ['<id>', ';']),
-    6: ('<id>', ['a']),
-    7: ('<id>', ['b'])
+    1: ('programa', ['<id>', ';', '<corpo>']),  # Regra 1: programa -> <id> ; <corpo>
+    2: ('<corpo>', ['begin', '<sentencas>', 'end']),  # Regra 2: <corpo> -> begin <sentencas> end
+    3: ('<sentencas>', ['<sentencas>', '<sentenca>']),  # Regra 3: <sentencas> -> <sentencas> <sentenca>
+    4: ('<sentencas>', []),  # Regra 4: <sentencas> -> epsilon (não possui símbolos)
+    5: ('<sentenca>', ['<id>', ';']),  # Regra 5: <sentenca> -> <id> ;
+    6: ('<id>', ['a']),  # Regra 6: <id> -> a
+    7: ('<id>', ['b'])  # Regra 7: <id> -> b
 }
 
-
-# Função de análise sintática
 def analisador_sintatico(frase):
     pilha = [0]  # Pilha de estados
     entrada = frase.split() + ['$']  # Símbolos de entrada
 
     while True:
-        estado_atual = pilha[-1]
-        simbolo_atual = entrada[0]
+        estado_atual = pilha[-1]  # Obtém o estado atual
+        simbolo_atual = entrada[0]  # Obtém o próximo símbolo de entrada
 
         if (estado_atual, simbolo_atual) not in tabela:
-            return False  # Erro de análise
+            return False  # Erro de análise: ação não definida na tabela
 
-        acao = tabela[(estado_atual, simbolo_atual)]
+        acao = tabela[(estado_atual, simbolo_atual)]  # Obtém a ação da tabela
 
-        if acao.startswith('s'):
-            novo_estado = int(acao[1:])
-            pilha.append(simbolo_atual)
-            pilha.append(novo_estado)
-            entrada = entrada[1:]
-        elif acao.startswith('r'):
-            regra_numero = int(acao[1:])
-            regra = regras[regra_numero]
-            simbolo_regra = regra[0]
-            tamanho_regra = len(regra[1])
+        if acao.startswith('s'):  # Se a ação for um "shift"
+            novo_estado = int(acao[1:])  # Obtém o novo estado
+            pilha.append(simbolo_atual)  # Empilha o símbolo atual
+            pilha.append(novo_estado)  # Empilha o novo estado
+            entrada = entrada[1:]  # Avança para o próximo símbolo de entrada
+        elif acao.startswith('r'):  # Se a ação for uma "reduce"
+            regra_numero = int(acao[1:])  # Obtém o número da regra
+            regra = regras[regra_numero]  # Obtém a regra correspondente ao número
+            simbolo_regra = regra[0]  # Obtém o símbolo não terminal da regra
+            tamanho_regra = len(regra[1])  # Obtém o tamanho da regra (número de símbolos)
 
-            for _ in range(2 * tamanho_regra):
+            for _ in range(2 * tamanho_regra):  # Desempilha 2 * tamanho_regra símbolos da pilha
                 pilha.pop()
 
-            estado_atual = pilha[-1]
-            pilha.append(simbolo_regra)
-            pilha.append(int(tabela[(estado_atual, simbolo_regra)]))
+            estado_atual = pilha[-1]  # Obtém o estado atual
+            pilha.append(simbolo_regra)  # Empilha o símbolo da regra
+            pilha.append(int(tabela[(estado_atual, simbolo_regra)]))  # Empilha o novo estado após a redução
 
-        elif acao == 'accept':
+        elif acao == 'accept':  # Se a ação for "accept"
             return True  # Análise sintática bem-sucedida
 
 # Exemplo de uso
